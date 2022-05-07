11672f3d569ef363fd5f57efbc54036eae8dbc57
koki <rayzymoon@yahoo.co.jp>
2022-05-07 11:20:55 +0900

    whitelist追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.hosts << "https://docker-test-awsome.herokuapp.com/"
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
